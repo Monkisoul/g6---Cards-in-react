fae25360aca65636064b78ed153ddf5c9098edf1
Monkisoul <jalegc@hotmail.com>
2021-06-24 16:29:51 -0500

    failtofetchfromlocal

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import './App.css';
+
+
+class Deck extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      cards: []
+    }
+  }
+
+  componentDidMount() {
+    (async () => {
+      const cards = await (await fetch(`http://localhost:4001/${this.props.path}`)).json()
+      this.setState({ cards })
+      console.log("hola", cards)
+
+    })();
+  }
+  render() {
+    console.log("mike", this.state.cards)
+    console.log(this.state.cards[`{this.props.path === "Table" ? "deck" : "hands"}`])
+    return <div>
+      {
+        (this.state.cards.length === 0) ?
+          <div className="loader"></div>
+          :
+          <div>
+            <h3> {`${this.props.path === "Table" ? "Table" : "Yor Hand"}`} </h3>
+            <div className="deck">
+              {
+
+                this.state.cards[`${this.props.path === "table" ? "deck" : "hand"}`].map((card, index) => {
+                  const number = card.slice(0, -1)
+                  const symbol = card.slice(-1)
+
+                  const isNumber = !isNaN(number)
+
+                  return <div className="card" number={number}>
+                    <div className="container">
+                      <div className="back"></div>
+                      <div className="front">
+                        <div className="card-corner top-left">
+                          <div>{number}</div>
+                          <div>{symbol}</div>
+                        </div>
+                        <div className="symbols">
+                          {number === 'A' ? <div> {symbol} </div> : ""}
+                          {isNumber ? new Array(parseInt(number))
+                            .fill(symbol)
+                            .map(
+                              (cardSymbol) => <div>{cardSymbol}</div>)
+                            : ""
+                          }
+                          {
+                            number === 'J' || number === 'Q' || number === 'K' ?
+                              <div className="image"></div>
+                              : ""
+                          }
+                        </div>
+                        <div className="card-corner bottom-right">
+                          <div>{number}</div>
+                          <div>{symbol}</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                })
+              }
+            </div>
+          </div>
+      }
+    </div>
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Deck title="Table" path="table" />
+        <Deck title="Hand" path="hand" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
